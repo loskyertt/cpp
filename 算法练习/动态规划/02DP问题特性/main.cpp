091ccc00ae5fa1d91d3c3fea1d01dc7da50b44e0
loskyertt <loskyertt0403@gmail.com>
2024-11-15 11:35:24 +0800

    temp

--- 算法练习/动态规划/02DP问题特性/main.cpp
+++ 算法练习/动态规划/02DP问题特性/main.cpp
@@ -4,10 +4,13 @@
 
 int main()
 {
-    vector<int> cost = { 0, 1, 10, 1 };
-    int min_cost = minCostClimbingStairsDP(cost);
+    // vector<int> cost = { 0, 1, 10, 1 };
+    // int min_cost = minCostClimbingStairsDP(cost);
 
-    cout << "最小花费是：" << min_cost << endl;
+    // cout << "最小花费是：" << min_cost << endl;
+
+    int ways = climbingStairsConstraintDP(3);
+    cout << "有" << ways << "种方式。" << endl;
 
     return 0;
 }
