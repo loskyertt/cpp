65fd94c505fcefdfc0424708e2ad3de9b7eb3bb2
loskyertt <loskyertt0403@gmail.com>
2025-03-17 11:44:44 +0800

    tmp

---
+++ 数据结构/06排序/交换类排序/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/* 打印数组 */
+void print_vector(vector<int> nums) {
+  for (int num : nums) {
+    cout << num << " ";
+  }
+  cout << endl;
+}
+
+/* 冒泡排序 */
+void bubble_sort(vector<int> &nums) {
+  for (int i = 0; i < nums.size() - 1; i++) { // i是每次减少比较的元素个数。最后一个元素不需要比较
+    bool swapped = false;                     // 如果元素是有序的，最终的状态一定还是 false，那么就可以直接退出循环
+    for (int j = 0; j < nums.size() - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        swap(nums[j], nums[j + 1]);
+        swapped = true;
+      }
+    }
+
+    if (!swapped) {
+      break;
+    }
+  }
+}
+
+/* 希尔排序 */
+void shell_sort(vector<int> &nums) {
+}
+
+/* 测试 */
+void test_01() {
+  vector<int> nums = {13, 6, 3, 31, 9, 27, 5, 11, 2, 15};
+
+  cout << "初始数组：";
+  print_vector(nums);
+
+  cout << "冒泡排序后的数组：";
+  bubble_sort(nums);
+  print_vector(nums);
+}
+
+int main() {
+  test_01();
+
+  return 0;
+}
--- 数据结构/06排序/插入类排序/main.cpp
+++ 数据结构/06排序/插入类排序/main.cpp
@@ -13,6 +13,7 @@ void print_vector(vector<int> nums) {
 }
 
 /* 直接插入排序：方式一 */
+// 效率比较低
 void insert_sort_directly_01(vector<int> &nums) {
   for (int i = 1; i < nums.size(); i++) {
     for (int j = i - 1; j >= 0; j--) {
@@ -29,9 +30,10 @@ void insert_sort_directly_01(vector<int> &nums) {
 /* 直接插入排序：方式二 */
 void insert_sort_directly_02(vector<int> &nums) {
   for (int i = 1; i < nums.size(); i++) {
+    // 记录下 nums[i] 的值
     int tmp = nums[i];
-    int j = i - 1;
 
+    int j = i - 1;
     // 移动比 tmp 大的元素
     while (nums[j] > tmp && j >= 0) {
       nums[j + 1] = nums[j];
@@ -43,9 +45,40 @@ void insert_sort_directly_02(vector<int> &nums) {
   }
 }
 
+/* 折半插入排序 */
+void insert_sort_binary(vector<int> &nums) {
+  for (int i = 1; i < nums.size(); i++) {
+    // 如果 nums[i] >= nums[i-1] ，直接去下一个数，因为前面的数都是有序的
+    if (nums[i] >= nums[i - 1]) {
+      continue;
+    }
+    int tmp = nums[i];
+    int left = 0;
+    int right = i - 1;
+    // 通过折半查找，先定位到 tmp 的位置
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] <= tmp) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    // 把从 left 开始的元素都往后移动，给 tmp 腾出空间
+    int j = i;
+    while (j > left) {
+      nums[j] = nums[j - 1];
+      j--;
+    }
+
+    // 插入 tmp，tmp 一定是插到 left 处
+    nums[left] = tmp;
+  }
+}
+
 /* 测试 */
 void test_01() {
-  vector<int> nums = {13, 6, 3, 31, 9, 27, 5, 11};
+  vector<int> nums = {13, 6, 3, 31, 9, 27, 5, 11, 2};
 
   cout << "初始数组：";
   print_vector(nums);
@@ -58,7 +91,7 @@ void test_01() {
 
 /* 测试 */
 void test_02() {
-  vector<int> nums = {13, 6, 3, 31, 9, 27, 5, 11};
+  vector<int> nums = {13, 6, 3, 31, 9, 27, 5, 11, 2};
 
   cout << "初始数组：";
   print_vector(nums);
@@ -69,9 +102,23 @@ void test_02() {
   print_vector(nums);
 }
 
+/* 测试 */
+void test_03() {
+  vector<int> nums = {13, 6, 3, 31, 9, 27, 5, 11, 2};
+
+  cout << "初始数组：";
+  print_vector(nums);
+
+  insert_sort_binary(nums);
+
+  cout << "折半插入排序后的数组：";
+  print_vector(nums);
+}
+
 int main() {
   test_01();
   test_02();
+  test_03();
 
   return 0;
 }
